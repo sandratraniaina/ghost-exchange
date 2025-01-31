b3cda3f31c173b3fd91b013e478bd520f0f2b031
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-31 16:03:38 +0300

    fix: Fix fucking table user to account.

--- server/src/main/sql/schema.sql
+++ server/src/main/sql/schema.sql
@@ -8,11 +8,19 @@ CREATE TYPE transaction_type AS ENUM (
    'REFUND'  
 );
 
+CREATE TABLE account(
+  id serial NOT NULL,
+  fiat_balance numeric(10, 2) NOT NULL,
+  username varchar(100) NOT NULL UNIQUE,
+  email varchar(100) NOT NULL,
+  CONSTRAINT account_pkey PRIMARY KEY(id)
+);
+
 CREATE TABLE cryptocurrency(
   id serial NOT NULL,
   "name" varchar(50) NOT NULL,
   symbol varchar(3) NOT NULL,
-  fiat_price numeric(18, 2) NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
   CONSTRAINT cryptocurrency_pkey PRIMARY KEY(id)
 );
 
@@ -20,7 +28,7 @@ CREATE TABLE cryptocurrency_wallet(
   id serial NOT NULL,
   user_id integer NOT NULL,
   cryptocurrency_id integer NOT NULL,
-  balance numeric(10, 12) NOT NULL DEFAULT 0,
+  balance numeric(10, 2) NOT NULL DEFAULT 0,
   CONSTRAINT cryptocurrency_wallet_pkey PRIMARY KEY(id)
 );
 
@@ -36,8 +44,8 @@ CREATE TABLE sell_order(
   id serial NOT NULL,
   seller_id integer NOT NULL,
   cryptocurrency_id integer NOT NULL,
-  amount numeric(18, 2) NOT NULL,
-  fiat_price numeric(18, 2) NOT NULL,
+  amount numeric(10, 12) NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
   "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   is_open bool NOT NULL DEFAULT true,
   CONSTRAINT sell_order_pkey PRIMARY KEY(id)
@@ -46,25 +54,17 @@ CREATE TABLE sell_order(
 CREATE TABLE "transaction"(
   id serial NOT NULL,
   user_id integer NOT NULL,
-  amount numeric(18, 2) NOT NULL,
+  amount numeric(10, 2) NOT NULL,
   transaction_type transaction_type NOT NULL,
   "timestamp" timestamp NOT NULL,
   validation_timestamp timestamp DEFAULT NULL,
   CONSTRAINT transaction_pkey PRIMARY KEY(id)
 );
 
-CREATE TABLE "user"(
-  id serial NOT NULL,
-  fiat_balance numeric(18, 2) NOT NULL,
-  username varchar(100) NOT NULL UNIQUE,
-  email varchar(100) NOT NULL,
-  CONSTRAINT user_pkey PRIMARY KEY(id)
-);
-
 CREATE TABLE xe_history(
   id serial NOT NULL,
   cryptocurrency_id integer NOT NULL,
-  fiat_price numeric(18, 2) NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
   "timestamp" timestamp NOT NULL,
   CONSTRAINT xe_history_pkey PRIMARY KEY(id)
 );
@@ -75,11 +75,11 @@ ALTER TABLE ledger
 
 ALTER TABLE sell_order
   ADD CONSTRAINT sell_order_seller_id_fkey
-    FOREIGN KEY (seller_id) REFERENCES "user" (id);
+    FOREIGN KEY (seller_id) REFERENCES account (id);
 
 ALTER TABLE ledger
   ADD CONSTRAINT ledger_buyer_id_fkey
-    FOREIGN KEY (buyer_id) REFERENCES "user" (id);
+    FOREIGN KEY (buyer_id) REFERENCES account (id);
 
 ALTER TABLE sell_order
   ADD CONSTRAINT sell_order_cryptocurrency_id_fkey
@@ -87,11 +87,11 @@ ALTER TABLE sell_order
 
 ALTER TABLE "transaction"
   ADD CONSTRAINT transaction_user_id_fkey
-    FOREIGN KEY (user_id) REFERENCES "user" (id);
+    FOREIGN KEY (user_id) REFERENCES account (id);
 
 ALTER TABLE cryptocurrency_wallet
   ADD CONSTRAINT cryptocurrency_wallet_user_id_fkey
-    FOREIGN KEY (user_id) REFERENCES "user" (id);
+    FOREIGN KEY (user_id) REFERENCES account (id);
 
 ALTER TABLE cryptocurrency_wallet
   ADD CONSTRAINT cryptocurrency_wallet_cryptocurrency_id_fkey
