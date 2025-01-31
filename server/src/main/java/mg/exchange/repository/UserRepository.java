9ccef3d6ee86e2cfab6c2804bb240ded7e7a8971
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-31 14:18:12 +0300

    feat: Create getUserByUsername function.

--- server/src/main/java/mg/exchange/repository/UserRepository.java
+++ server/src/main/java/mg/exchange/repository/UserRepository.java
@@ -3,9 +3,17 @@ package mg.exchange.repository;
 
 import mg.exchange.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 
 }
