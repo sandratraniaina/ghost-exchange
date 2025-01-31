1d601b97ff760ddc55dd58c542737491c705a385
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-31 13:58:11 +0300

    feat: Create User model , repository and service.

---
+++ server/src/main/java/mg/exchange/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package mg.exchange.repository;
+
+
+import mg.exchange.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
