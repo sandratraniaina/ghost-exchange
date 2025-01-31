ff616288d73789e79cb05c469455141f0b1e8da4
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-31 13:33:48 +0300

    feat : create class UserController

---
+++ server/src/main/java/mg/exchange/controllers/UserController.java
@@ -0,0 +1,10 @@
+package mg.exchange.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+}
