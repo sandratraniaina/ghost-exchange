4bc408716e0aa28a593bdba3ee04e9a58999dacd
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-31 14:22:40 +0300

    feat : create function sign in on controller

--- server/src/main/java/mg/exchange/controllers/UserController.java
+++ server/src/main/java/mg/exchange/controllers/UserController.java
@@ -1,10 +1,32 @@
 package mg.exchange.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import mg.exchange.models.Response;
+import mg.exchange.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     
+    @PostMapping("/sign-in")
+    public <T> ResponseEntity<Response<T>> signIn(@RequestParam String user){ 
+        try {
+            
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "User sign in successfully", null);
+        } catch (Exception e) {
+            ResponseUtil.sendResponse(
+                HttpStatus.BAD_REQUEST, 
+                false, 
+                "Error while attnullempting to sign", 
+                e.getMessage()
+            );
+        }
+        return null;
+    }
 }
