496fd1d34ecc445150bf89750a31e48b8dd8e6aa
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-31 14:43:33 +0300

    feat : add UserService on UserController

--- server/src/main/java/mg/exchange/controllers/UserController.java
+++ server/src/main/java/mg/exchange/controllers/UserController.java
@@ -1,5 +1,6 @@
 package mg.exchange.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import mg.exchange.models.Response;
+import mg.exchange.services.UserService;
 import mg.exchange.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
     
     @PostMapping("/sign-in")
     public <T> ResponseEntity<Response<T>> signIn(@RequestParam String user){ 
