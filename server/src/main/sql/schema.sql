158cd87f7bbb3de144ec1ede7f7f96cc8e70de65
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-31 11:43:25 +0300

    feat: Update script.

--- server/src/main/sql/schema.sql
+++ server/src/main/sql/schema.sql
@@ -2,98 +2,101 @@ SET check_function_bodies = false
 ;
 
 CREATE TYPE transaction_type AS ENUM (
-    'DEBIT',
-    'CREDIT',
-    'TRANSFER',
-    'REFUND'
-    );
+   'DEBIT',   
+   'CREDIT',  
+   'TRANSFER',
+   'REFUND'  
+);
 
 CREATE TABLE cryptocurrency(
-                               id serial NOT NULL,
-                               "name" varchar(50) NOT NULL,
-                               symbol varchar(3) NOT NULL,
-                               fiat_price numeric(10, 2) NOT NULL,
-                               CONSTRAINT cryptocurrency_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  "name" varchar(50) NOT NULL,
+  symbol varchar(3) NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
+  CONSTRAINT cryptocurrency_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE cryptocurrency_wallet(
-                                      id serial NOT NULL,
-                                      user_id integer NOT NULL,
-                                      cryptocurrency_id integer NOT NULL,
-                                      balance numeric(10, 12) NOT NULL DEFAULT 0,
-                                      CONSTRAINT cryptocurrency_wallet_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  user_id integer NOT NULL,
+  cryptocurrency_id integer NOT NULL,
+  balance numeric(10, 12) NOT NULL DEFAULT 0,
+  CONSTRAINT cryptocurrency_wallet_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE ledger(
-                       id serial NOT NULL,
-                       sell_order_id integer NOT NULL,
-                       buyer_id integer NOT NULL,
-                       "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       CONSTRAINT ledger_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  sell_order_id integer NOT NULL,
+  buyer_id integer NOT NULL,
+  "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT ledger_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE sell_order(
-                           id serial NOT NULL,
-                           seller_id integer NOT NULL,
-                           cryptocurrency_id integer NOT NULL,
-                           amount numeric(10, 12) NOT NULL,
-                           fiat_price numeric(10, 2) NOT NULL,
-                           "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           is_open bool NOT NULL DEFAULT true,
-                           CONSTRAINT sell_order_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  seller_id integer NOT NULL,
+  cryptocurrency_id integer NOT NULL,
+  amount numeric(10, 12) NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
+  "timestamp" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  is_open bool NOT NULL DEFAULT true,
+  CONSTRAINT sell_order_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE "transaction"(
-                              id serial NOT NULL,
-                              user_id integer NOT NULL,
-                              amount numeric(10, 2) NOT NULL,
-                              transaction_type transaction_type NOT NULL,
-                              "timestamp" timestamp NOT NULL,
-                              validation_timestamp timestamp DEFAULT NULL,
-                              CONSTRAINT transaction_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  user_id integer NOT NULL,
+  amount numeric(10, 2) NOT NULL,
+  transaction_type transaction_type NOT NULL,
+  "timestamp" timestamp NOT NULL,
+  validation_timestamp timestamp DEFAULT NULL,
+  CONSTRAINT transaction_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE "user"(
-                       id serial NOT NULL, fiat_balance numeric(10, 2) NOT NULL,
-                       CONSTRAINT user_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  fiat_balance numeric(10, 2) NOT NULL,
+  username varchar(100) NOT NULL UNIQUE,
+  email varchar(100) NOT NULL,
+  CONSTRAINT user_pkey PRIMARY KEY(id)
 );
 
 CREATE TABLE xe_history(
-                           id serial NOT NULL,
-                           cryptocurrency_id integer NOT NULL,
-                           fiat_price numeric(10, 2) NOT NULL,
-                           "timestamp" timestamp NOT NULL,
-                           CONSTRAINT xe_history_pkey PRIMARY KEY(id)
+  id serial NOT NULL,
+  cryptocurrency_id integer NOT NULL,
+  fiat_price numeric(10, 2) NOT NULL,
+  "timestamp" timestamp NOT NULL,
+  CONSTRAINT xe_history_pkey PRIMARY KEY(id)
 );
 
 ALTER TABLE ledger
-    ADD CONSTRAINT ledger_sell_order_id_fkey
-        FOREIGN KEY (sell_order_id) REFERENCES sell_order (id);
+  ADD CONSTRAINT ledger_sell_order_id_fkey
+    FOREIGN KEY (sell_order_id) REFERENCES sell_order (id);
 
 ALTER TABLE sell_order
-    ADD CONSTRAINT sell_order_seller_id_fkey
-        FOREIGN KEY (seller_id) REFERENCES "user" (id);
+  ADD CONSTRAINT sell_order_seller_id_fkey
+    FOREIGN KEY (seller_id) REFERENCES "user" (id);
 
 ALTER TABLE ledger
-    ADD CONSTRAINT ledger_buyer_id_fkey
-        FOREIGN KEY (buyer_id) REFERENCES "user" (id);
+  ADD CONSTRAINT ledger_buyer_id_fkey
+    FOREIGN KEY (buyer_id) REFERENCES "user" (id);
 
 ALTER TABLE sell_order
-    ADD CONSTRAINT sell_order_cryptocurrency_id_fkey
-        FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
+  ADD CONSTRAINT sell_order_cryptocurrency_id_fkey
+    FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
 
 ALTER TABLE "transaction"
-    ADD CONSTRAINT transaction_user_id_fkey
-        FOREIGN KEY (user_id) REFERENCES "user" (id);
+  ADD CONSTRAINT transaction_user_id_fkey
+    FOREIGN KEY (user_id) REFERENCES "user" (id);
 
 ALTER TABLE cryptocurrency_wallet
-    ADD CONSTRAINT cryptocurrency_wallet_user_id_fkey
-        FOREIGN KEY (user_id) REFERENCES "user" (id);
+  ADD CONSTRAINT cryptocurrency_wallet_user_id_fkey
+    FOREIGN KEY (user_id) REFERENCES "user" (id);
 
 ALTER TABLE cryptocurrency_wallet
-    ADD CONSTRAINT cryptocurrency_wallet_cryptocurrency_id_fkey
-        FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
+  ADD CONSTRAINT cryptocurrency_wallet_cryptocurrency_id_fkey
+    FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
 
 ALTER TABLE xe_history
-    ADD CONSTRAINT xe_history_cryptocurrency_id_fkey
-        FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
+  ADD CONSTRAINT xe_history_cryptocurrency_id_fkey
+    FOREIGN KEY (cryptocurrency_id) REFERENCES cryptocurrency (id);
