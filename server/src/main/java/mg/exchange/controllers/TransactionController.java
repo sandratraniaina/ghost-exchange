b43d9d17250983c23909ab2421f0cbb35d7d98ab
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-01 19:51:34 +0300

    feat : create endpoint : POST /api/transactions

--- server/src/main/java/mg/exchange/controllers/TransactionController.java
+++ server/src/main/java/mg/exchange/controllers/TransactionController.java
@@ -1,10 +1,18 @@
 package mg.exchange.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mg.exchange.models.Response;
+import mg.exchange.models.Transaction;
 import mg.exchange.services.TransactionService;
+import mg.exchange.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -13,4 +21,17 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @SuppressWarnings("unchecked")
+    @GetMapping
+    public <T> ResponseEntity<Response<T>> getAllTransactions(){
+        try {
+            List<Transaction> transactions = transactionService.getAllTransactions();
+            if(transactions.size() == 0){
+                return ResponseUtil.sendResponse(HttpStatus.OK, true, "List of transaction fetched successfully but empty", (T) transactions);
+            }
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "List of transactions fetched successfully", (T)transactions);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error while fetching transactions data", (T)e.getMessage());
+        }
+    }
 }
