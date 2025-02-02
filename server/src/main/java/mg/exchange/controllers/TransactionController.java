848e29a69442004653c8bbe2f129f9680b4dd6ba
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-02 10:04:50 +0300

    feat : create enpoint : GET /api/users/{userId}/sell-orders?type=

--- server/src/main/java/mg/exchange/controllers/TransactionController.java
+++ server/src/main/java/mg/exchange/controllers/TransactionController.java
@@ -1,6 +1,5 @@
 package mg.exchange.controllers;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- server/src/main/java/mg/exchange/controllers/UserController.java
+++ server/src/main/java/mg/exchange/controllers/UserController.java
@@ -1,6 +1,7 @@
 package mg.exchange.controllers;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import mg.exchange.dto.SignInRequest;
 import mg.exchange.models.Response;
+import mg.exchange.models.SellOrder;
 import mg.exchange.models.User;
+import mg.exchange.services.SellOrderService;
 import mg.exchange.services.UserService;
 import mg.exchange.utils.ResponseUtil;
 
@@ -26,6 +29,10 @@ public class UserController {
     @Autowired
     private UserService userService;
     
+    @Autowired 
+    private SellOrderService sellOrderService;
+    
+    @SuppressWarnings("unchecked")
     @PostMapping("/sign-in")
     public <T> ResponseEntity<Response<T>> signIn(@RequestBody SignInRequest user) { 
         try {
@@ -40,7 +47,29 @@ public class UserController {
             return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error while attempting to sign in", (T)e.getMessage());
         }
     }
-    
 
+    @SuppressWarnings("unchecked")
+    @GetMapping("/{userId}/sell-orders")
+    public <T> ResponseEntity<Response<T>> getUserSellOrders(@PathVariable Long userId, @RequestParam(required = false) String type){
+        try {
+            Optional<User> u = userService.getUserById(userId);
+            if(u == null){
+                throw new Exception("User not found for id : "+userId);
+            }
+            List<SellOrder> sellOrders;
+            if (type != null) {
+                if (type.trim().toLowerCase().equals("open")) {
+                    sellOrders = sellOrderService.getOpenSellOrdersBySellerId(userId);
+                } else {
+                    throw new IllegalArgumentException("Unknown type of sell order: " + type);
+                }
+            } else {
+                sellOrders = sellOrderService.getSellOrdersBySellerId(userId);
+            } 
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Sell orders fetched successfully", (T) sellOrders);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error while attempting to fetch sell orders for user "+userId, (T)e.getMessage());
+        }
+    }
 }
  
