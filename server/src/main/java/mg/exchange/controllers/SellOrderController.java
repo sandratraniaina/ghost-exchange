53f9db268cd8876ddf226bac15a7743c77b81956
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-02 07:50:28 +0300

    feat : create endpoint : POST /api/sell-orders

--- server/src/main/java/mg/exchange/controllers/SellOrderController.java
+++ server/src/main/java/mg/exchange/controllers/SellOrderController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,20 @@ public class SellOrderController {
             return ResponseUtil.sendResponse(HttpStatus.OK, true, "Sell orders fetched successfully", (T) sellOrders);
         } catch (Exception e) {
             return ResponseUtil.sendResponse(HttpStatus.INTERNAL_SERVER_ERROR, false, "Error while fetching sell orders", (T) e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @PostMapping
+    public <T> ResponseEntity<Response<T>> createSellOrder(@RequestBody SellOrder sellOrder){
+        try {
+            if(sellOrder == null){
+                throw new Exception("Cannot create a sell order of a value null");
+            }
+            sellOrder = sellOrderService.createSellOrder(sellOrder);
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Sell order created successfully", (T) sellOrder);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error while creating sell order", (T) e.getMessage());
         }
     }
     
