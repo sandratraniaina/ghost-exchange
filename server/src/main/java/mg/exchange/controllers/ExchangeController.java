c6d70c3b6ec5e2df65d8e570917e77c3c89c9764
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-02 20:33:47 +0300

    feat : create endpoint GET /api/exchanges

--- server/src/main/java/mg/exchange/controllers/ExchangeController.java
+++ server/src/main/java/mg/exchange/controllers/ExchangeController.java
@@ -1,10 +1,18 @@
 package mg.exchange.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mg.exchange.models.Response;
+import mg.exchange.models.XeHistory;
 import mg.exchange.services.XeHistoryService;
+import mg.exchange.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/api/exchanges")
@@ -12,5 +20,15 @@ public class ExchangeController {
     
     @Autowired
     private XeHistoryService exchangeService;
-    
+
+    @SuppressWarnings("unchecked")
+    @GetMapping
+    public <T> ResponseEntity<Response<T>> getAllExchanges(){
+        try {
+            List<XeHistory> exchanges = exchangeService.getAllXeHistories();
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Exchanges history fetched successfully", (T)exchanges);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.INTERNAL_SERVER_ERROR, false, "Error while fetching exchanges", (T) e.getMessage());
+        }
+    }
 }
