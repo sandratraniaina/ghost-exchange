d529642cb0f9745b845f738ea0ec0fb886e4862f
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-03 11:47:51 +0300

    fix: Fix some bug.

--- server/src/main/java/mg/exchange/services/AnalysisResultService.java
+++ server/src/main/java/mg/exchange/services/AnalysisResultService.java
@@ -1,11 +1,9 @@
 package mg.exchange.services;
 
 import lombok.RequiredArgsConstructor;
-import mg.exchange.repository.AnalysisResultRepsository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AnalysisResultService {
-    private final AnalysisResultRepsository analysisResultRepository;
 }
