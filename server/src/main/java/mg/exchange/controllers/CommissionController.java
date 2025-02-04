49a1aa6aedbcc1ffec977fc8776832c485af0729
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-04 17:55:38 +0300

    feat: Create endpoint for searching the commissionSummary.

--- server/src/main/java/mg/exchange/controllers/CommissionController.java
+++ server/src/main/java/mg/exchange/controllers/CommissionController.java
@@ -1,13 +1,19 @@
 package mg.exchange.controllers;
 
+import mg.exchange.dto.CommissionSummaryDTO;
 import mg.exchange.models.Commission;
 import mg.exchange.models.Response;
 import mg.exchange.services.CommissionService;
 import mg.exchange.utils.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/commissions")
@@ -25,6 +31,22 @@ public class CommissionController {
             return ResponseUtil.sendResponse(HttpStatus.OK, true, "Commission updated successfully", (T) updatedCommission);
         } catch (Exception e) {
             return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error updating commission", (T) e.getMessage());
+        }
+    }
+
+    @PostMapping("/search")
+    public <T> ResponseEntity<Response<T>> getCommissionSummary(
+            @RequestParam(required = false) Long cryptocurrencyId,
+            @RequestParam String typeAnalyse,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime min,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime max) {
+        try {
+            Timestamp minTimestamp = (min != null) ? Timestamp.valueOf(min) : null;
+            Timestamp maxTimestamp = (max != null) ? Timestamp.valueOf(max) : null;
+            List<CommissionSummaryDTO> summary = commissionService.getCommissionSummary(cryptocurrencyId, typeAnalyse, minTimestamp, maxTimestamp);
+            return ResponseUtil.sendResponse(HttpStatus.OK, true, "Commission summary retrieved successfully", (T) summary);
+        } catch (Exception e) {
+            return ResponseUtil.sendResponse(HttpStatus.BAD_REQUEST, false, "Error retrieving commission summary", (T) e.getMessage());
         }
     }
 }
