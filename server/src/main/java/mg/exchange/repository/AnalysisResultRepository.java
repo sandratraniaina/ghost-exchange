4216a33751a133cdf670a85b408e587ad63fa649
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-04 16:38:44 +0300

    feat : Create function getFirstQuartile on AnalysisResultRepository

--- server/src/main/java/mg/exchange/repository/AnalysisResultRepository.java
+++ server/src/main/java/mg/exchange/repository/AnalysisResultRepository.java
@@ -50,6 +50,18 @@ public interface AnalysisResultRepository extends JpaRepository<Cryptocurrency, 
         @Param("cryptos") List<Cryptocurrency> cryptos,
         @Param("min") LocalDateTime min,
         @Param("max") LocalDateTime max);
+
+    @Query("SELECT new mg.exchange.dto.AnalysisResult(c, PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY x.fiatPrice)) " +
+        "FROM Cryptocurrency c " +
+        "JOIN XeHistory x ON c.id = x.cryptocurrency.id " +
+        "WHERE c IN :cryptos " +
+        "AND (cast(:min as timestamp) IS NULL OR x.timestamp >= :min) " +
+        "AND (cast(:max as timestamp) IS NULL OR x.timestamp <= :max) " +
+        "GROUP BY c")
+    List<AnalysisResult> getFirstQuartile(
+        @Param("cryptos") List<Cryptocurrency> cryptos,
+        @Param("min") LocalDateTime min,
+        @Param("max") LocalDateTime max);
   
 }
 
