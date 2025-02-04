800270cd6664f25a2024ca5fb8277b768141a770
hasinajy <jyhasina@gmail.com>
2025-02-04 15:22:05 +0300

    test: Display CryptoChart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import FiatTransaction from './components/FiatTransaction/FiatTransaction';
 import UserProfile from './components/UserProfile/UserProfile';
+import CryptoChart from './components/CryptoChart/CryptoChart';
 
 function App() {
   return (
     <>
       <FiatTransaction />
       <UserProfile />
+      <CryptoChart />
     </>
   )
 }
