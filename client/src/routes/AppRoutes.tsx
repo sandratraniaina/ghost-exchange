60ab321bf5dfed32569bde6f56cad62fe0de867a
sandratraniaina <sandratrarafa@gmail.com>
2025-02-04 14:43:13 +0300

    feat: Add LoginPage component to the route

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '@/components/layouts/MainLayout';
 import { DashboardPage } from '@/components/pages/DashboardPage';
+import { LoginPage } from '@/components/pages/auth/LoginPage';
 
 export const AppRoutes = () => {
     return (
         <Routes>
+            {/* Auth routes without layout */}
+            <Route path="/login" element={<LoginPage />} />
+
             {/* Public routes with main layout */}
             <Route element={<MainLayout />}>
                 <Route path="/" element={<DashboardPage />} />
             </Route>
+
         </Routes>
     );
 };
