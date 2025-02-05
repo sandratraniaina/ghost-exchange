4672761d326674ce0aab0ef181a7abafe883d9dd
sandratraniaina <sandratrarafa@gmail.com>
2025-02-05 17:34:27 +0300

    feat: Wrap main component inside ThemeProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { AppRoutes } from './routes/AppRoutes';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </AuthProvider>
-  );
+	return (
+		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<AuthProvider>
+				<BrowserRouter>
+					<AppRoutes />
+				</BrowserRouter>
+			</AuthProvider>
+		</ThemeProvider>
+	);
 };
 
 export default App;
