8ba57f5a05319259b6e8fb3524e090256236bc67
hasinajy <jyhasina@gmail.com>
2025-02-05 15:07:53 +0300

    fix: Provide default value for User

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,14 +1,16 @@
 import { SignupFormData } from "@/components/pages/auth/SignupPage";
 
-export interface User {
-    id: string;
-    name: string;
-    email: string;
+export class User {
+    id: string = "1";
+    name: string = "John Doe";
+    email: string = "johndoe@gmail.com";
+    avatar: string = "https://randomuser.me/api/portraits";
+    role: string = "User";
 }
 
 export interface AuthContextType {
     isAuthenticated: boolean;
-    user: User | null;
+    user: User;
     login: (email: string, password: string) => Promise<void>;
     signup: (user: SignupFormData) => Promise<void>;
     logout: () => void;
