d2b7cb8b5f754b5bf6d98f81d3f839badddc5c73
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-05 15:31:06 +0300

    feat : Create function getUserTransactionSummary on service.

--- server/src/main/java/mg/exchange/services/UserTransactionSummaryService.java
+++ server/src/main/java/mg/exchange/services/UserTransactionSummaryService.java
@@ -1,9 +1,12 @@
 package mg.exchange.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import mg.exchange.dto.UserTransactionSummary;
 import mg.exchange.repository.UserTransactionSummaryRepository;
 
 @Service
@@ -12,4 +15,9 @@ public class UserTransactionSummaryService {
     
     @Autowired
     private UserTransactionSummaryRepository userTransactionRepository;
+
+    public List<UserTransactionSummary> getUserTransactionSummary() {
+        return userTransactionRepository.getUserTransactionSummary();
+    }
+
 }
