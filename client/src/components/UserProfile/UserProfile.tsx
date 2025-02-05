81097eb5ba2018cff9543ddf9831410dfb55870d
sandratraniaina <sandratrarafa@gmail.com>
2025-02-05 17:34:00 +0300

    feat: Add darkmode toggle to user profile

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -1,24 +1,26 @@
 import { useAuth } from '@/hooks/useAuth';
 import { LogOut } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import DarkModeToggle from '../layouts/DarkModeToggle';
 
 const UserProfile = () => {
   const { user, logout } = useAuth();
 
   return (
     <div className="inline-flex items-center space-x-2 bg-white rounded-lg p-2 shadow-sm min-w-fit">
+      <DarkModeToggle></DarkModeToggle>
       <Avatar>
         <AvatarImage src={user.avatar} alt={user.name} />
         <AvatarFallback>{user.name.split(' ').map(n => n[0]).join('')}</AvatarFallback>
       </Avatar>
-      
+
       <div className="text-left whitespace-nowrap">
         <div className="font-medium">{user.name}</div>
         <div className="text-sm text-gray-500">{user.role}</div>
       </div>
-      
-      <LogOut 
-        className="h-5 w-5 text-gray-500 ml-2 cursor-pointer hover:text-red-500 transition-colors" 
+
+      <LogOut
+        className="h-5 w-5 text-gray-500 ml-2 cursor-pointer hover:text-red-500 transition-colors"
         onClick={() => { logout() }}
       />
     </div>
