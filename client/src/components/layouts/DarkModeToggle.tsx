c4239280140dced34c83be89a6c6073fcccc97c5
sandratraniaina <sandratrarafa@gmail.com>
2025-02-05 17:33:32 +0300

    feat: Craete DarkModeToggle component

---
+++ client/src/components/layouts/DarkModeToggle.tsx
@@ -0,0 +1,20 @@
+import { Moon, Sun } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "@/hooks/userTheme";
+
+const DarkModeToggle = () => {
+	const { theme, setTheme } = useTheme();
+	const isDark = theme === "dark";
+
+	return (
+		<button
+			onClick={() => setTheme(isDark ? "light" : "dark")}
+			className="flex items-center gap-2 px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-800 transition-all"
+		>
+			{isDark ? <Moon className="h-5 w-5 text-yellow-400" /> : <Sun className="h-5 w-5 text-gray-800" />}
+			<Switch checked={isDark} onCheckedChange={() => setTheme(isDark ? "light" : "dark")} />
+		</button>
+	);
+};
+
+export default DarkModeToggle;
