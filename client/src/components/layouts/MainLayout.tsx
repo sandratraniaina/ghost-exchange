7c4ee899be77b31867f9835286da4c9e9c47c749
hasinajy <jyhasina@gmail.com>
2025-02-05 15:11:39 +0300

    feat: Protect main layout with an auth

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -2,17 +2,20 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '@/components/layouts/Header';
 import { Sidebar } from '@/components/layouts/Sidebar';
 import { BaseProps } from '@/types/common';
+import { ProtectedRoute } from '../ProtectedRoute';
 
 export const MainLayout: React.FC<BaseProps> = ({ className = '' }) => {
     return (
-        <div className={`min-h-screen flex flex-col w-full h-full ${className}`}>
-            <Header />
-            <div className="flex flex-1 h-full">
-                <Sidebar />
-                <main className="flex-1 p-6">
-                    <Outlet />
-                </main>
+        <ProtectedRoute>
+            <div className={`min-h-screen flex flex-col w-full h-full ${className}`}>
+                <Header />
+                <div className="flex flex-1 h-full">
+                    <Sidebar />
+                    <main className="flex-1 p-6">
+                        <Outlet />
+                    </main>
+                </div>
             </div>
-        </div>
+        </ProtectedRoute>
     );
 };
