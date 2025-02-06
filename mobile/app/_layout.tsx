6942b321f373cc33d52a606ad36896aaf3937034
sandratraniaina <sandratrarafa@gmail.com>
2025-02-06 17:54:13 +0300

    feat: Update main app layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,16 @@
-import { createTamagui, TamaguiProvider, View } from 'tamagui';
-import { defaultConfig } from "@tamagui/config/v4";
 import { Stack } from 'expo-router';
+import { createTamagui, TamaguiProvider } from 'tamagui';
+import { defaultConfig } from "@tamagui/config/v4";
 
 const config = createTamagui(defaultConfig);
 
-export default () => (
-	<TamaguiProvider config={config}>
-		<Stack />
-	</TamaguiProvider>
-)
+export default function Layout() {
+    return (
+        <TamaguiProvider config={config}>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="login" options={{ presentation: 'modal' }} />
+            </Stack>
+        </TamaguiProvider>
+    );
+}
