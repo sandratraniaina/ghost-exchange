e8fb07501244d134ecf7c3badd75d72ef30baa07
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-06 21:31:30 +0300

    feat: Implement Transaction to FirestoreSyncable.

--- server/src/main/java/mg/exchange/models/Transaction.java
+++ server/src/main/java/mg/exchange/models/Transaction.java
@@ -1,18 +1,20 @@
 package mg.exchange.models;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mg.exchange.utils.FirestoreSyncable;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "transaction")
-public class Transaction {
+public class Transaction implements FirestoreSyncable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +36,28 @@ public class Transaction {
 
     @Column(name = "validation_timestamp")
     private LocalDateTime validationTimestamp;
+
+    @Override
+    public String getFirestoreCollectionName() {
+        return "transaction";
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Map<String, Object> toFirestoreMap() {
+        Map<String, Object> map = new HashMap<>();
+        
+        if (id != null) map.put("id", id);
+        if (user != null) map.put("user", user.toFirestoreMap());
+        if (amount != null) map.put("amount", amount);
+        if (transactionType != null) map.put("transactionType", transactionType.name());
+        if (timestamp != null) map.put("timestamp", timestamp.toString());
+        if (validationTimestamp != null) map.put("validationTimestamp", validationTimestamp.toString());
+        
+        return map;
+    }
 }
