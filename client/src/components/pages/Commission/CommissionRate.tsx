e5d1aec9c6f06ba6c793b74f2c151fd397c2f0bd
hasinajy <jyhasina@gmail.com>
2025-02-06 13:28:29 +0300

    feat: Create a new route for commission rate update

---
+++ client/src/components/pages/Commission/CommissionRate.tsx
@@ -0,0 +1,5 @@
+export const CommissionRate = () => {
+    return (
+        <></>
+    );
+};
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -6,6 +6,7 @@ import { SignupPage } from '@/components/pages/auth/SignupPage';
 import { Wallet } from '@/components/pages/Wallet/Wallet';
 import { Marketplace } from '@/components/pages/Marketplace/Marketplace';
 import { Analysis } from '@/components/pages/Analysis/Analysis';
+import { CommissionRate } from '@/components/pages/Commission/CommissionRate';
 
 export const AppRoutes = () => {
     return (
@@ -20,6 +21,7 @@ export const AppRoutes = () => {
                 <Route path="/wallet" element={<Wallet />} />
                 <Route path="/marketplace" element={<Marketplace />} />
                 <Route path="/analysis" element={<Analysis />} />
+                <Route path="/commission-rate" element={<CommissionRate />} />
             </Route>
 
         </Routes>
