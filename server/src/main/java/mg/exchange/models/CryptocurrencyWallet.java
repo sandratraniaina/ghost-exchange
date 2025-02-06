cc9cca6ca720f8d99098f9df38c6bac1fe68da6c
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-06 21:30:51 +0300

    feat: Implement CryptocurrencyWallet to FirestoreSyncable.

--- server/src/main/java/mg/exchange/models/CryptocurrencyWallet.java
+++ server/src/main/java/mg/exchange/models/CryptocurrencyWallet.java
@@ -3,14 +3,17 @@ package mg.exchange.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mg.exchange.utils.FirestoreSyncable;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "cryptocurrency_wallet")
-public class CryptocurrencyWallet {
+public class CryptocurrencyWallet implements FirestoreSyncable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +29,37 @@ public class CryptocurrencyWallet {
 
     @Column(nullable = false, precision = 18, scale = 2, columnDefinition = "numeric(18, 2) DEFAULT 0")
     private BigDecimal balance;
+
+    @Override
+    public String getFirestoreCollectionName() {
+        return "cryptocurrency_wallet";
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+public Map<String, Object> toFirestoreMap() {
+    Map<String, Object> map = new HashMap<>();
+
+    if (id != null) {
+        map.put("id", id);
+    }
+    
+    if (user != null) {
+        map.put("user", user.toFirestoreMap());
+    }
+    
+    if (cryptocurrency != null) {
+        map.put("cryptocurrency", cryptocurrency.toFirestoreMap());
+    }
+    
+    if (balance != null) {
+        map.put("balance", balance);
+    }
+
+    return map;
+}
 }
