89ae7e92fc99027a0d26c28ebca089868e4ef5fb
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-06 21:28:26 +0300

    feat: Implement Cryptocurrency to FirestoreSyncable.

--- server/src/main/java/mg/exchange/models/Cryptocurrency.java
+++ server/src/main/java/mg/exchange/models/Cryptocurrency.java
@@ -1,17 +1,19 @@
 package mg.exchange.models;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mg.exchange.utils.FirestoreSyncable;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "cryptocurrency")
-public class Cryptocurrency {
+public class Cryptocurrency implements FirestoreSyncable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +27,30 @@ public class Cryptocurrency {
 
     @Column(name = "fiat_price", nullable = false, precision = 18, scale = 2)
     private BigDecimal fiatPrice;
+
+    @Override
+    public String getFirestoreCollectionName() {
+        return "cryptocurrency";
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Map<String, Object> toFirestoreMap() {
+        Map<String, Object> map = new HashMap<>();
+
+        if (id != null)
+            map.put("id", id);
+        if (name != null)
+            map.put("name", name);
+        if (symbol != null)
+            map.put("symbol", symbol);
+        if (fiatPrice != null)
+            map.put("fiatPrice", fiatPrice);
+
+        return map;
+    }
 }
