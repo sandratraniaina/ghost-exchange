89b7008ec9c9f65af476ef57d5d26fef54ef58ad
sandratraniaina <sandratrarafa@gmail.com>
2025-02-06 17:09:28 +0300

    chore: Install tamagui and add test to the project

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { createTamagui, TamaguiProvider, View } from 'tamagui';
+import { defaultConfig } from "@tamagui/config/v4";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const config = createTamagui(defaultConfig);
+
+export default () => (
+	<TamaguiProvider config={config}>
+		<Stack />
+	</TamaguiProvider>
+)
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
+
+import { Activity, Airplay } from '@tamagui/lucide-icons';
+import { Button, XGroup, XStack, YStack } from 'tamagui';
+
+import { createTamagui, TamaguiProvider } from 'tamagui';
+import { defaultConfig } from "@tamagui/config/v4";
+
+const config = createTamagui(defaultConfig);
+
+export function ButtonDemo() {
+    return (
+        <YStack padding="$3" gap="$3" >
+            <Button>Plain</Button>
+            <Button alignSelf="center" icon={Airplay} size="$6">
+                Large
+            </Button>
+            <XStack gap="$2" justifyContent="center">
+                <Button size="$3" theme="active">
+                    Active
+                </Button>
+                <Button size="$3" variant="outlined">
+                    Outlined
+                </Button>
+            </XStack>
+            <XStack gap="$2">
+                <Button themeInverse size="$3">
+                    Inverse
+                </Button>
+                <Button iconAfter={Activity} size="$3">
+                    iconAfter
+                </Button>
+            </XStack>
+            <XGroup>
+                <XGroup.Item>
+                    <Button width="50%" size="$2" disabled opacity={0.5}>
+                        disabled
+                    </Button>
+                </XGroup.Item>
+
+                <XGroup.Item>
+                    <Button width="50%" size="$2" chromeless>
+                        chromeless
+                    </Button>
+                </XGroup.Item>
+            </XGroup>
+        </YStack>
+    )
+}
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <TamaguiProvider config={config}>
+            <ButtonDemo></ButtonDemo>
+        </TamaguiProvider>
+    );
 }
