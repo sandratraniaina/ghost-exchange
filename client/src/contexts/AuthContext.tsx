b03c5f1b1a96bfdbdd9be377a6abe9bab4355d38
sandratraniaina <sandratrarafa@gmail.com>
2025-02-07 20:56:07 +0300

    feat: Implement login function

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -9,18 +9,32 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const [user, setUser] = useLocalStorage<User | null>('user', null);
 
     const login = async (email: string, password: string) => {
-        // Mock successful API login
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        // Create a new User instance with the provided email
-        const mockUser = new User();
-        mockUser.email = email;
-        mockUser.name = email.split('@')[0].replace(/[^a-zA-Z0-9]/g, ' '); // Convert email to name
-        mockUser.id = Math.random().toString(36).substr(2, 9); // Random ID
-        mockUser.avatar = `https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 100)}.jpg`;
-        mockUser.role = "User";
+        try {
+            const host = import.meta.env.VITE_ANONYMIZER;
+            console.log("Lasa izy zay");
+            const response = await fetch(`${host}/auth/signin`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email, password})
+            });
 
-        setUser(mockUser);
-        return true;
+            console.log(response);
+
+            const data = await response.json();
+
+            if (data.success) {
+                return true;
+            } else {
+                throw new Error(data.message);
+            }
+
+        } catch (error) { // Type the error as any or Error
+            console.error("Signup error:", error);
+
+            throw error;
+        }
     };
 
     const signup = async (formData: SignupFormData) => {
