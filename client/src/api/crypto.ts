8232ca050bcd55376188533fb6752243ab314dc3
hasinajy <jyhasina@gmail.com>
2025-02-07 16:31:18 +0300

    feat: Create fetchCryptoOptions()

---
+++ client/src/api/crypto.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const fetchCryptoOptions = async () => {
+  try {
+    const apiHost = process.env.REACT_APP_API_HOST;
+    if (!apiHost) {
+      throw new Error('REACT_APP_API_HOST environment variable is not defined.');
+    }
+
+    const uri = '/cryptocurrencies';
+    const url = `http://${apiHost}${uri}`;
+
+    const response = await axios.get(url);
+    return response.data.json();
+
+  } catch (error) {
+    console.error('Error fetching cryptocurrencies:', error);
+    return null;
+  }
+};
