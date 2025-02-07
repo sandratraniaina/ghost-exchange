bf1fd01400172f732d0e4020b7b0743484bbce42
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-07 19:03:20 +0300

    feat : Create function insertUser(User u) on FirebaseService.

--- server/src/main/java/mg/exchange/services/FirebaseService.java
+++ server/src/main/java/mg/exchange/services/FirebaseService.java
@@ -2,17 +2,39 @@ package mg.exchange.services;
 
 import org.springframework.stereotype.Service;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
 import lombok.RequiredArgsConstructor;
+import mg.exchange.models.User;
 
 @Service
 @RequiredArgsConstructor
 public class FirebaseService {
+
     private final FirebaseMessaging firebaseMessaging;
+    private final FirebaseAuth firebaseAuth;
+
+    public FirebaseAuth getFirebaseAuth() {
+        return firebaseAuth;
+    }
+
+    public String insertUser(User u) throws Exception {
+        CreateRequest request = new CreateRequest()
+                .setEmail(u.getEmail())
+                .setEmailVerified(false)
+                .setPassword("DefaultPassword123!") // Ensure the password is securely managed
+                .setDisplayName(u.getUsername())
+                .setDisabled(false);
+        
+        UserRecord userRecord = firebaseAuth.createUser(request);
+        return userRecord.getUid();
+    }
 
     public String sendNotification(String title, String body, String token) throws FirebaseMessagingException {
         Notification notification = Notification.builder()
