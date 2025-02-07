11be9723a5129bcbd77c146da2a37b7ad4b5e3d8
sandratraniaina <sandratrarafa@gmail.com>
2025-02-08 02:53:40 +0300

    feat: Create validatePIN function

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,21 +1,20 @@
-export const validatePIN = async (pin: string,  userId: string) => {
+import { User } from "@/types/auth";
+
+export const validatePIN = async (pin: string, userId: string) => {
     try {
         const host = import.meta.env.VITE_ANONYMIZER;
-        console.log("Lasa izy zay");
         const response = await fetch(`${host}/auth/signin/${userId}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ "PIN": pin}),
+            body: JSON.stringify({ PIN: pin }),
         });
-
-        console.log(response);
 
         const data = await response.json();
 
         if (data.success) {
-            return true;
+            return data;
         } else {
             throw new Error(data.message);
         }
