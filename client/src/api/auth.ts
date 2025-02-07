e0c86ad7a2f1fb52486d76bf6a0300fcc5fb312d
sandratraniaina <sandratrarafa@gmail.com>
2025-02-08 02:54:05 +0300

    feat: Crearte sendUserDate function

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -25,3 +25,35 @@ export const validatePIN = async (pin: string, userId: string) => {
         throw error;
     }
 };
+
+export const sendUserData = async (user: User) => {
+    try {
+        const host = import.meta.env.VITE_API_HOST;
+        const response = await fetch(`${host}/users/sign-in`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": user.email,
+                "username": user.username,
+                "pwd": user.password,
+            }),
+        });
+
+        console.log(response);
+
+        const data = await response.json();
+
+        if (data.success) {
+            return true;
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        // Type the error as any or Error
+        console.error("PIN Validation error:", error);
+
+        throw error;
+    }
+};
