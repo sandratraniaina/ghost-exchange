84b6220b3ccb9e040cbe3dab4502c27da280effc
hasinajy <jyhasina@gmail.com>
2025-02-07 22:46:12 +0300

    feat: Create getUserWallet()

---
+++ client/src/api/wallet.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const getUserWallet = async (userId: number) => {
+    try {
+        const apiHost = import.meta.env.VITE_API_HOST;
+
+        if (!apiHost) {
+            throw new Error('VITE_API_HOST environment variable is not defined.');
+        }
+
+        const uri = `/wallets${userId}/wallets`;
+        const url = `http://${apiHost}${uri}`;
+
+        const response = await axios.post(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user wallet:', error);
+        return null;
+    }
+};
