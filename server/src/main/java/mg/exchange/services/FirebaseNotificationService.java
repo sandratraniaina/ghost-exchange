87693a14e44bc28af24bf241668ceda1cef5a408
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-02-07 17:34:48 +0300

    feat : Create FirebaseNotificationService

---
+++ server/src/main/java/mg/exchange/services/FirebaseNotificationService.java
@@ -0,0 +1,30 @@
+package mg.exchange.services;
+
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FirebaseNotificationService {
+    private final FirebaseMessaging firebaseMessaging;
+
+    public String sendNotification(String title, String body, String token) throws FirebaseMessagingException {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        Message message = Message.builder()
+                .setToken(token)
+                .setNotification(notification)
+                .build();
+
+        return firebaseMessaging.send(message);
+    }
+}
