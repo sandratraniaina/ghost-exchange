be260b6f4263c63e6daa10a2f4e57e6a304c2386
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-08 20:08:15 +0300

    fix: Change the function getCommissionSummar to getCommissionSummarByCryptoCurrencyId.

--- server/src/main/java/mg/exchange/repository/CommissionRepository.java
+++ server/src/main/java/mg/exchange/repository/CommissionRepository.java
@@ -14,7 +14,8 @@ import java.util.List;
 public interface CommissionRepository extends JpaRepository<Commission, Long> {
     @Query(value = """
             SELECT
-                so.cryptocurrency_id ,
+                so.cryptocurrency_id AS cryptocurrency_id,
+            
                 -- Conditional SUM or AVG for sales commission
                 CASE
                     WHEN :type_analyse = 'sum' THEN SUM(l.sales_commission * so.amount * so.fiat_price)
@@ -30,11 +31,15 @@ public interface CommissionRepository extends JpaRepository<Commission, Long> {
             FROM ledger l
                      JOIN sell_order so ON l.sell_order_id = so.id
             WHERE (:cryptocurrency_id IS NULL OR so.cryptocurrency_id = :cryptocurrency_id)
-              AND (cast(:min as timestamp) IS NULL OR so.timestamp >= :min)
-              AND (cast(:max as timestamp) IS NULL OR so.timestamp <= :max)
+              AND (CAST(:min AS TIMESTAMP) IS NULL OR so.timestamp >= :min)
+              AND (CAST(:max AS TIMESTAMP) IS NULL OR so.timestamp <= :max)
             
-            GROUP BY
-                so.cryptocurrency_id
+            GROUP BY so.cryptocurrency_id;
             """, nativeQuery = true)
-    List<CommissionSummaryDTO> getCommissionSummary(@Param("cryptocurrency_id") Long cryptocurrencyId, @Param("type_analyse") String type_analyse, @Param("min") Timestamp min, @Param("max") Timestamp max);
+    CommissionSummaryDTO getCommissionSummarByCryptoCurrencyId(
+            @Param("cryptocurrency_id") Long cryptocurrencyId,
+            @Param("type_analyse") String type_analyse,
+            @Param("min") Timestamp min,
+            @Param("max") Timestamp max);
+
 }
