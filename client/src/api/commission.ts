ce741258a45754914f23abced6f51f991e2fd5e2
hasinajy <jyhasina@gmail.com>
2025-02-08 15:46:27 +0300

    feat: Create updateCommissions()

--- client/src/api/commission.ts
+++ client/src/api/commission.ts
@@ -18,3 +18,27 @@ export const fetchCommissions = async () => {
     return null;
   }
 };
+
+export const updateCommissions = async (salesCommission: number, purchaseCommission: number) => {
+  try {
+    const apiHost = import.meta.env.VITE_API_HOST;
+
+    if (!apiHost) {
+      throw new Error('VITE_API_HOST environment variable is not defined.');
+    }
+
+    const uri = '/commissions/1';
+    const url = `http://${apiHost}${uri}`;
+
+    const requestBody = {
+      salesCommission: salesCommission,
+      purchasesCommission: purchaseCommission
+    };
+
+    const response = await axios.put(url, requestBody);
+    return response.data;
+  } catch (error) {
+    console.error('Error performing transaction operation:', error);
+    return null;
+  }
+}
