9e04400e9b075c3931f3f3e332b7757bdb8cb98f
sandratraniaina <sandratrarafa@gmail.com>
2025-02-08 15:53:47 +0300

    feat: Create declineTransaction function

--- client/src/api/dashboard.ts
+++ client/src/api/dashboard.ts
@@ -5,7 +5,7 @@ export const getOpenTransaction = async () => {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
-            }
+            },
         });
 
         console.log(response);
@@ -20,6 +20,33 @@ export const getOpenTransaction = async () => {
     } catch (error) {
         // Type the error as any or Error
         console.error("Error while fetching transaction data", error);
+
+        throw error;
+    }
+};
+
+export const declineTransaction = async (transactionId: number) => {
+    try {
+        const host = import.meta.env.VITE_API_HOST;
+        const response = await fetch(`http://${host}/transactions/${transactionId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        console.log(response);
+
+        const data = await response.json();
+
+        if (data.success) {
+            return true;
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        // Type the error as any or Error
+        console.error("Error while declining a transaction", error);
 
         throw error;
     }
