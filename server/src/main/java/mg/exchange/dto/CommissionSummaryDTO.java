badbc580a385d26da2ec03143f3044de87bde9d3
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-08 20:09:37 +0300

    fix: Add annotation AllArgsConstructor.

--- server/src/main/java/mg/exchange/dto/CommissionSummaryDTO.java
+++ server/src/main/java/mg/exchange/dto/CommissionSummaryDTO.java
@@ -1,10 +1,14 @@
 package mg.exchange.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommissionSummaryDTO {
     private Long cryptocurrencyId;
     private BigDecimal totalSalesCommission;
