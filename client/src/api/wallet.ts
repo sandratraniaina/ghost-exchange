7b6d906d5305f5dc425cb92f055b76385be536ee
hasinajy <jyhasina@gmail.com>
2025-02-08 17:29:55 +0300

    feat: Create fetchUserBalance()

--- client/src/api/wallet.ts
+++ client/src/api/wallet.ts
@@ -1,5 +1,28 @@
 import axios from 'axios';
 
+export const fetchUserBalance = async (userId: number) => {
+    try {
+        const apiHost = import.meta.env.VITE_API_HOST;
+
+        if (!apiHost) {
+            throw new Error('VITE_API_HOST environment variable is not defined.');
+        }
+
+        const uri = `/users/balances`;
+        const url = `http://${apiHost}${uri}`;
+
+        const requestBody = {
+            userId: userId
+        }
+
+        const response = await axios.post(url, requestBody);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user wallet:', error);
+        return null;
+    }
+}
+
 export const getUserWallet = async (userId: number) => {
     try {
         const apiHost = import.meta.env.VITE_API_HOST;
